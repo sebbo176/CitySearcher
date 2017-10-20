4de667dd97cb8aec2a25e1dcb14b7d39db85ab43
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2017-10-20 21:23:08 +0200

    initial commit. Add podfile and RxCocoa and RxSwift 🦈

---
+++ CitySearcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CitySearcher
+//
+//  Created by Sebastian Bolling on 2017-10-20.
+//  Copyright © 2017 Sebastian Bolling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'CitySearcher' do
+
+pod "RxSwift"
+pod "RxCocoa"
+
+end
