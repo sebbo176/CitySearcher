4d7d6ed39d06e0b1e8a7ab1a742ead84c27cc407
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2017-10-25 20:58:44 +0200

    Add Outlets for searchbar and tableview. Hardcoded data

--- CitySearcher/ViewController.swift
+++ CitySearcher/ViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Our mocked API data source
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +33,16 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
+
+        return cell
+    }
+}
